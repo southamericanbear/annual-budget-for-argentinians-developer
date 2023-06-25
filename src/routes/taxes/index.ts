8f357fa88fd61a65113bcf2706470461e64c5c31
southamericanbear <dittler.a@gmail.com>
2023-06-25 17:59:31 -0300

    refactor how we count and read the files

--- src/routes/taxes/index.ts
+++ src/routes/taxes/index.ts
@@ -18,24 +18,13 @@ routes.get('/get-taxes-stats', async (req, res) => {
 	}
 });
 
-routes.get('/get-invoices', async (req, res) => {
-	const firebaseService = new FirebaseService();
-	const { folder } = req.query;
-	try {
-		const invoices = await firebaseService.getFiles(folder as string);
-		if (!invoices.length) return res.status(404).send('no invoices found');
-		return res.status(200).send(invoices);
-	} catch (error) {
-		return error;
-	}
-});
-
 routes.post('/upload-invoice', upload.array('invoices'), async (req, res) => {
+	const { year, month } = req.query;
 	const invoices = req.files as Express.Multer.File[];
 	const firebaseService = new FirebaseService();
 	try {
-		const urls = await firebaseService.uploadFiles('invoices', 'june', '2023', invoices);
-		return res.status(200).json({ message: 'invoices uploaded', urls });
+		await firebaseService.uploadFiles('invoices', month as string, year as string, invoices);
+		return res.status(200).json({ message: 'invoices uploaded' });
 	} catch (error) {
 		return error;
 	}
--- src/utils/taxes/index.ts
+++ src/utils/taxes/index.ts
@@ -1,34 +1,44 @@
-import { promises as fsPromises } from 'fs';
-import { resolve as pathResolve } from 'path';
 import pdf from 'pdf-parse';
+import FirebaseService from '../../services/firebase-service';
+import axios from 'axios';
+
 export const getTotalTaxesMonth = async (year: string, month: string): Promise<{ numberOfInovices: number; totalInvoices: number }> => {
-	const invoiceDir = `./local/${year}/${month}`;
-	const invoiceFiles = await fsPromises.readdir(invoiceDir);
+	try {
+		const firebaseService = new FirebaseService();
+		const invoicesUrls = await firebaseService.getFiles(`invoices/${year}/${month}`);
 
-	let subtotalNumber = 0;
+		if (!invoicesUrls.length) {
+			return {
+				numberOfInovices: 0,
+				totalInvoices: 0,
+			};
+		}
 
-	await Promise.all(
-		invoiceFiles.map(async (fileName) => {
-			const invoicePath = pathResolve(invoiceDir, fileName);
-			const dataBuffer = await fsPromises.readFile(invoicePath);
-			const { text } = await pdf(dataBuffer);
+		let subtotalNumber = 0;
 
-			const lines = text.split('\n');
-			for (const line of lines) {
-				if (line.includes('Subtotal: $')) {
-					const subtotalLineIndex = lines.indexOf(line) - 1;
-					const parsedNumber = parseFloat(lines[subtotalLineIndex].replace(',', '.'));
-					subtotalNumber += parsedNumber;
-					break;
+		await Promise.all(
+			invoicesUrls.map(async (invoiceUrl) => {
+				const { data } = await axios(invoiceUrl, { responseType: 'arraybuffer' });
+				const buffer = Buffer.from(data, 'binary');
+				const { text } = await pdf(buffer);
+				const lines = text.split('\n');
+
+				for (const line of lines) {
+					if (line.includes('Subtotal: $')) {
+						const subtotalLineIndex = lines.indexOf(line) - 1;
+						const parsedNumber = parseFloat(lines[subtotalLineIndex].replace(',', '.'));
+						subtotalNumber += parsedNumber;
+						break;
+					}
 				}
-			}
-		})
-	).catch((error) => {
-		throw error;
-	});
+			})
+		);
 
-	return {
-		numberOfInovices: invoiceFiles.length,
-		totalInvoices: subtotalNumber,
-	};
+		return {
+			numberOfInovices: invoicesUrls.length,
+			totalInvoices: subtotalNumber,
+		};
+	} catch (error) {
+		return error;
+	}
 };
