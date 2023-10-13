ef2ff58ad64c4268c6cf764f3df9ea43cf8cdadf
southamericanbear <dittler.a@gmail.com>
2023-10-13 20:56:49 -0300

    updated budget routes

--- src/routes/budgets/index.ts
+++ src/routes/budgets/index.ts
@@ -1,30 +1,32 @@
 import { Router, Request, Response } from 'express';
 import { createBudgetValidation } from '../../utils';
 import { getBudgets, getBudgetById, getTotalBudget, createBudget, updateBudget, deleteBudget } from '../../controllers';
+import { jwtValidator } from '../../middlewares';
 const routes = Router();
 
-routes.get('/', async (req, res) => {
+routes.get('/', jwtValidator, async (req, res) => {
 	try {
-		const budgets = await getBudgets();
+		const budgets = await getBudgets(req.body.user.userId);
 		res.status(200).json({ message: 'Budgets', budgets });
 	} catch (error) {
 		res.status(500).json(error.message);
 	}
 });
 
-routes.get('/get-total', async (req, res) => {
+routes.get('/get-total', jwtValidator, async (req, res) => {
 	try {
-		const totalBudget = await getTotalBudget();
+		const totalBudget = await getTotalBudget(req.body.user.userId);
 		res.status(200).json({ message: 'Total budget', totalBudget });
 	} catch (error) {
 		res.status(500).json(error.message);
 	}
 });
 
-routes.get('/:budgetId', async (req, res) => {
+routes.get('/:budgetId', jwtValidator, async (req, res) => {
 	const { budgetId } = req.params;
+
 	try {
-		const budget = await getBudgetById(budgetId);
+		const budget = await getBudgetById(budgetId, req.body.user.userId);
 		res.status(200).json({ message: 'Budget', budget });
 	} catch (error) {
 		res.status(500).json(error.message);
@@ -34,6 +36,7 @@ routes.get('/:budgetId', async (req, res) => {
 routes.post('/', createBudgetValidation, async (req: Request, res: Response) => {
 	try {
 		const { body } = req;
+		delete body.user;
 		const budget = await createBudget(body);
 		res.status(200).json({ message: 'Budget created', budget });
 	} catch (error) {
@@ -41,21 +44,24 @@ routes.post('/', createBudgetValidation, async (req: Request, res: Response) => 
 	}
 });
 
-routes.put('/:budgetId', async (req, res) => {
+routes.put('/:budgetId', jwtValidator, async (req, res) => {
 	const { budgetId } = req.params;
 	const { body } = req;
+	const userId = req.body.user.userId;
+	delete body.user;
+
 	try {
-		const budget = await updateBudget(budgetId, body);
+		const budget = await updateBudget(budgetId, body, userId);
 		res.status(200).json({ message: 'Budget updated', budget });
 	} catch (error) {
 		res.status(500).json(error.message);
 	}
 });
 
-routes.delete('/:budgetId', async (req, res) => {
+routes.delete('/:budgetId', jwtValidator, async (req, res) => {
 	const { budgetId } = req.params;
 	try {
-		const budgetDeletedId = await deleteBudget(budgetId);
+		const budgetDeletedId = await deleteBudget(budgetId, req.body.user.userId);
 		return res.status(200).json({ message: 'Budget deleted', budgetDeletedId });
 	} catch (error) {
 		res.status(500).json(error.message);
