46e92810789dda00c4fe80479e7c62ee562b16a7
southamericanbear <dittler.a@gmail.com>
2023-12-05 21:42:38 -0300

    update relative paths

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,53 @@
 import express from 'express';
 import cors from 'cors';
-import { auth, base, basicData, budgets, dollar, taxes } from './routes';
-import { getDollarBlueValue, updateDollarBlueValue } from './routes/dollar/use-cases/dollar-blue-value';
+import { auth, base, basicData, budgets, dollar, taxes, getDollarBlueValue, updateDollarBlueValue } from '@routes/index';
+// import { getDollarBlueValue, updateDollarBlueValue } from './routes/dollar/use-cases/dollar-blue-value';
 // import { updateScheduleWeekDays } from './services/update-schedule';
 // import { keepAlive } from './services/keep-alive';
 
 class App {
-	public server;
-	public paths;
+  public server;
+  public paths;
 
-	constructor() {
-		this.server = express();
-		this.middlewares();
-		this.paths = {
-			base: '/',
-			auth: '/api/auth',
-			dollar: '/api/dollar',
-			taxes: '/api/taxes',
-			basicInfo: '/api/basic-data',
-			budgets: '/api/budgets',
-		};
-		this.routes();
-		//	keepAlive();
-		// updateScheduleWeekDays(600000);
-		this.updateDollarBlue();
-	}
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.paths = {
+      base: '/',
+      auth: '/api/auth',
+      dollar: '/api/dollar',
+      taxes: '/api/taxes',
+      basicInfo: '/api/basic-data',
+      budgets: '/api/budgets',
+    };
+    this.routes();
+    //	keepAlive();
+    // updateScheduleWeekDays(600000);
+    this.updateDollarBlue();
+  }
 
-	middlewares() {
-		this.server.use(cors());
-		this.server.use(express.json());
-	}
+  middlewares() {
+    this.server.use(cors());
+    this.server.use(express.json());
+  }
 
-	routes() {
-		this.server.use(this.paths.auth, auth);
-		this.server.use(this.paths.dollar, dollar);
-		this.server.use(this.paths.taxes, taxes);
-		this.server.use(this.paths.basicInfo, basicData);
-		this.server.use(this.paths.budgets, budgets);
-		this.server.use(this.paths.base, base);
-	}
+  routes() {
+    this.server.use(this.paths.auth, auth);
+    this.server.use(this.paths.dollar, dollar);
+    this.server.use(this.paths.taxes, taxes);
+    this.server.use(this.paths.basicInfo, basicData);
+    this.server.use(this.paths.budgets, budgets);
+    this.server.use(this.paths.base, base);
+  }
 
-	async updateDollarBlue() {
-		const value = await getDollarBlueValue();
-		if (value) {
-			await updateDollarBlueValue();
-		} else {
-			return;
-		}
-	}
+  async updateDollarBlue() {
+    const value = await getDollarBlueValue();
+    if (value) {
+      await updateDollarBlueValue();
+    } else {
+      return;
+    }
+  }
 }
 
 export default new App().server;
