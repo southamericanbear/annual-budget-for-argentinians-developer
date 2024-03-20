3e4cd48cea029c546d83fdd4ece90aa2b75007b3
southamericanbear <dittler.a@gmail.com>
2024-03-20 16:48:52 -0300

    added jwt for signup

--- src/utils/signup-validators.ts
+++ src/utils/signup-validators.ts
@@ -1,5 +1,13 @@
 import { check } from 'express-validator';
-import { validationFields } from '../middlewares';
+import { jwtValidator, validationFields } from '../middlewares';
 import { emailExistsValidator } from './db-validators';
 
-export const signupValidators = [check('email').custom(emailExistsValidator), check('name', 'Name is required').not().isEmpty(), check('email', 'Email is required').not().isEmpty(), check('email', 'Invalid email').isEmail(), check('password', 'Password is required').not().isEmpty(), validationFields];
+export const signupValidators = [
+  jwtValidator,
+  check('email').custom(emailExistsValidator),
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Email is required').not().isEmpty(),
+  check('email', 'Invalid email').isEmail(),
+  check('password', 'Password is required').not().isEmpty(),
+  validationFields,
+];
